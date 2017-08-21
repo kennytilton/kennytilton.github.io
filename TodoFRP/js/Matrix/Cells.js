a43242f57c7d21aea1edc295d20569e86ac756ab
Kenneth Tilton <kentilton@gmail.com>
2017-08-20 23:56:33 -0400

    msCrypto for IE11

--- TodoFRP/js/Matrix/Cells.js
+++ TodoFRP/js/Matrix/Cells.js
@@ -25,8 +25,10 @@ function xor (a, b) {
 window['xor']=xor;
 
 function uuidv4() {
+	let cripto = window.crypto || window.msCrypto;
+
 	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-		(c ^ window.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16))
+		(c ^ cripto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16))
 }
 window['uuidv4'] = uuidv4;
 
