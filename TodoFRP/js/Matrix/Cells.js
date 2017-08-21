21eb88173b9cfafe9dcc508c9f4ae0a354b96131
Kenneth Tilton <kentilton@gmail.com>
2017-08-20 23:59:54 -0400

    Update Cells.js

--- TodoFRP/js/Matrix/Cells.js
+++ TodoFRP/js/Matrix/Cells.js
@@ -26,7 +26,7 @@ window['xor']=xor;
 
 function uuidv4() {
 	let cripto = window.crypto || window.msCrypto;
-
+        console.log('some cripto? '+ cripto + ' grv? '+ cripto.getRandomValues );
 	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
 		(c ^ cripto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16))
 }
